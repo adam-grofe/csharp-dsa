c12b5d923b859cfe8cb59297f6699d86a3605930
Adam Grofe <agrofe1417@gmail.com>
2025-03-09 08:07:54 -0700

    Add Binary Search.

---
+++ BinarySearch/BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace Binary.Search
+{
+
+  public class BinarySearchClass
+  {
+    public int BinarySearch( List<int> l, int val)
+    {
+      if (l == null) return -1;
+      Trace.WriteLine("Enter binary search");
+      int high = l.Count-1;
+      int low = 0;
+      while ( high >= low )
+      {
+        int mid = (high+low)/2;
+        Trace.WriteLine($"Mid = {mid}  High = {high}  Low = {low}");
+        if( l[mid] == val )
+        {
+          return mid;
+        }
+        else if ( l[mid] < val )
+        {
+          low = mid+1;
+        }
+        else if (l[mid] > val )
+        {
+          high = mid-1;
+        }
+      }
+      return -1;
+    }
+  }  
+}
---
+++ BinarySearchTests/BinarySearchUnitTest.cs
@@ -0,0 +1,65 @@
+using Xunit;
+using System.Diagnostics;
+using Binary.Search;
+
+namespace BinarySearchTests
+{
+    public class BinarySearchUnitTest
+    {
+        void setLogger(string testName){
+            string logPath = Path.Combine(Environment.CurrentDirectory, $"log-{testName}.txt");
+            TextWriterTraceListener logFile = new (File.CreateText(logPath));
+            Trace.Listeners.Add(logFile);
+            Trace.AutoFlush = true;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            setLogger("Test1");
+            var bs = new BinarySearchClass();
+            var l = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var val = 5;
+            var result = bs.BinarySearch(l, val);
+            Assert.Equal(4, result);
+        }
+        [Fact]
+        public void Test2()
+        {
+            setLogger("Test2");
+            var bs = new BinarySearchClass();
+            var l = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            for (int i = 0; i < l.Count; i++)
+            {
+                var val = l[i];
+                var result = bs.BinarySearch(l, val);
+                Assert.Equal(i, result);
+            }
+        }
+        [Fact]
+        public void Test3()
+        {
+            setLogger("Test3");
+            var bs = new BinarySearchClass();
+            var l = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var val = 20;
+            var result = bs.BinarySearch(l, val);
+            Assert.Equal(-1, result);
+           
+        }
+        [Fact]
+        public void Test4()
+        {
+            setLogger("Test4");
+            var bs = new BinarySearchClass();
+            var l = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+            for (int i = 0; i < l.Count; i++)
+            {
+                var val = l[i];
+                var result = bs.BinarySearch(l, val);
+                Assert.Equal(i, result);
+            }
+        }
+
+    }
+}
