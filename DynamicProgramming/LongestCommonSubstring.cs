1acb7a880b53162be074c66c8e306488981c5c25
Adam Grofe <agrofe1417@gmail.com>
2025-05-12 19:35:11 -0700

    Add implementation of longest common substring.

---
+++ DynamicProgramming/LongestCommonSubstring.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Reflection.Metadata.Ecma335;
+
+namespace DynamicProgramming;
+
+static public class LongestCommonSubstring
+{
+
+    static public int LCS_DPTabulation(string left, string right)
+    {
+        int m = left.Length;
+        int n = right.Length;
+
+        int[,] table = new int[m+1, n+1];
+
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (left[i - 1] == right[j - 1])
+                {
+                    // When they are same add one to the next element
+                    table[i, j] = table[i - 1, j - 1] + 1;
+                }
+                else
+                {
+                    // Else choose the highest out of the previous elements
+                    table[i, j] = Math.Max(table[i - 1, j], table[i, j - 1]);
+                }
+            }
+        }
+        return table[m, n];
+    }
+}
---
+++ DynamicProgrammingTests/LongestCommonSubstringTests.cs
@@ -0,0 +1,51 @@
+﻿using DynamicProgramming;
+
+namespace DynamicProgrammingTests;
+
+public class LongestCommonSubstringTests
+{
+    [Theory]
+    [InlineData("")]
+    [InlineData("ABC")]
+    [InlineData("ABCDEFGHIJKLMNOP")]
+    [InlineData("ABCDEFGHIJKLMNOPQRSTUVWXYZ")]
+    public void LongestCommonSubstring_MatchingStrings_ReturnsLength(string input)
+    {
+        // Arrange
+
+        // Act
+        var result = LongestCommonSubstring.LCS_DPTabulation(input, input);
+
+        // Assert
+        Assert.Equal(input.Length, result);
+    }
+
+    [Theory]
+    [InlineData("ABC", "ACD", 2)]
+    [InlineData("AGGTAB", "GXTXAYB", 4)]
+    [InlineData("ABC", "CBA", 1)]
+    public void LongestCommonSubstring_DifferentStrings_ReturnsInt(string left, string right, int expectedResult)
+    {
+        // Arrange
+
+        // Act
+        var result = LongestCommonSubstring.LCS_DPTabulation(left, right);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Theory]
+    [InlineData("ABCD", "EFGH")]
+    [InlineData("ABCDEFGH", "IJKLMNOPQ")]
+    public void LongestCommonSubstring_CompletelyDifferentStrings_Returns0(string left, string right)
+    {
+        // Arrange
+
+        // Act
+        var result = LongestCommonSubstring.LCS_DPTabulation(left, right);
+
+        // Assert
+        Assert.Equal(0, result);
+    }
+}
