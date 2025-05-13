9f9c9095adc8ada8c54e22e34c02e40c676f3650
Adam Grofe <agrofe1417@gmail.com>
2025-05-12 21:09:07 -0700

    Add knapsack problem

---
+++ DynamicProgramming/Knapsack.cs
@@ -0,0 +1,38 @@
+﻿namespace DynamicProgramming;
+public static class Knapsack
+{
+    public static int MaxProfit(List<int> profit, List<int> weight, int maxWeight)
+    {
+        if (profit.Count != weight.Count)
+        {
+            throw new ArgumentException("The sizes of the arrays are not the same.");
+        }
+
+        int n = profit.Count;
+
+        int[,] table = new int[n + 1, maxWeight + 1];
+        for (int i = 0; i <= n; i++)
+        {
+            table[i, 0] = 0;
+            table[0, i] = 0;
+        }
+        table[0, 0] = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= maxWeight; j++)
+            {
+                int pick = 0;
+
+                if (weight[i-1] <= j)
+                {
+                    pick = profit[i - 1] + table[i - 1, j - weight[i - 1]];
+                }
+                int nopick = table[i - 1, j];
+
+                table[i, j] = Math.Max(pick, nopick);
+            }
+        }
+        return table[n, maxWeight];
+    }
+}
---
+++ DynamicProgrammingTests/KnapsackTests.cs
@@ -0,0 +1,35 @@
+﻿using DynamicProgramming;
+
+namespace DynamicProgrammingTests;
+public class KnapsackTests
+{
+    static List<int> parseListString(string listStr)
+    {
+        var result = new List<int>();
+        foreach (var c in listStr)
+        {
+            string intStr = $"{c}";
+            result.Add(Convert.ToInt32(intStr));
+        }
+        return result;
+    }
+
+    [Theory]
+    [InlineData("123", "451", 4, 3)]
+    [InlineData("123", "451", 5, 4)]
+    [InlineData("123", "451", 6, 5)]
+    [InlineData("123", "451", 12, 6)]
+    [InlineData("2423", "1324", 9, 9)]
+    public void Knapsack_ValidArrays_ReturnProfit(string profitItems, string weightItems, int maxWeight, int expectedResult)
+    {
+        // Arrange
+        var profit = parseListString(profitItems);
+        var weight = parseListString(weightItems);
+
+        // Act
+        var result = Knapsack.MaxProfit(profit, weight, maxWeight);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
