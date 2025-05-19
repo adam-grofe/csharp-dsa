e3effd38a33f3ab31e13720642b05707df95f702
Adam Grofe <agrofe1417@gmail.com>
2025-05-18 21:23:15 -0700

    Add test to quick sort tests.

--- SortTests/QuickSortTests.cs
+++ SortTests/QuickSortTests.cs
@@ -36,7 +36,7 @@ public class QuickSortTests
     [InlineData("1236456", "1234566")]
     [InlineData("65241321", "11223456")]
     [InlineData("16452353", "12334556")]
-    [InlineData("11111111111111","11111111111111")]
+    [InlineData("11111111111111", "11111111111111")]
     public void QuickSort_WithDuplicateElements_Sorted(string inputStr, string expectedStr)
     {
         // Arrange
