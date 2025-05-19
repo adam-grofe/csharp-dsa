31970765b56b269d57c95ef7f843d1ab8961ba70
Adam Grofe <agrofe1417@gmail.com>
2025-05-18 21:22:36 -0700

    Add Min operation matrix multiplicaiton

---
+++ DynamicProgramming/MinOpMatrixMultiply.cs
@@ -0,0 +1,29 @@
+﻿namespace DynamicProgramming;
+public static class MinOpMatrixMultiply
+{
+    public static int MinOp(List<int> dims)
+    {
+        int N = dims.Count;
+
+        int[,] table = new int[N, N];
+
+        for (int len = 2; len < N; len++)
+        {
+            for (int i = 0; i < N - len; i++)
+            {
+                int j = i + len;
+                table[i, j] = int.MaxValue;
+
+                for (int k = i + 1; k < j; k++)
+                {
+                    int cost = table[i, k] + table[k, j] + dims[i] * dims[k] * dims[j];
+                    if (cost < table[i, j])
+                    {
+                        table[i, j] = cost;
+                    }
+                }
+            }
+        }
+        return table[0, N - 1];
+    }
+}
---
+++ DynamicProgrammingTests/MinOpMatrixMultiplyTests.cs
@@ -0,0 +1,35 @@
+﻿using DynamicProgramming;
+using System.Text;
+
+namespace DynamicProgrammingTests;
+public class MinOpMatrixMultiplyTests
+{
+    private List<int> parseStr(string inputStr)
+    {
+        var result = new List<int>();
+        var idxList = inputStr.Split(",").ToList();
+        foreach (var idx in idxList)
+        {
+            int i = Convert.ToInt32(idx.Trim());
+            result.Add( i );
+        }
+        return result;
+    }
+
+    [Theory]
+    [InlineData("2, 3", 0)]
+    [InlineData("2, 1, 3, 4", 20)]
+    [InlineData("1, 2, 3, 4, 3", 30)]
+    public void MinOp_ValidList_ReturnsExpectedResult(string inputStr, int expected)
+    {
+        // Arrange
+        List<int> input = parseStr(inputStr);
+
+        // Act
+        var result = MinOpMatrixMultiply.MinOp(input);
+
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
